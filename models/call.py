7a2c68faa6f2d27caaafae1ddae489faa6696d44
Peter Grigor <petrupewdiepie@gmail.com>
2025-06-08 14:42:58 +0300

    some changes

--- models/call.py
+++ models/call.py
@@ -4,6 +4,9 @@ class Call(db.Model):
     __tablename__ = 'call'
     id = db.Column(db.String(100), primary_key=True)
     from_phone = db.Column(db.String(100))
+    call_date = db.Column(db.DateTime, nullable=False)
+    to_number = db.Column(db.String(100), nullable=True)
+    to_contact_name = db.Column(db.String(200), nullable=True)
     
     recording_url = db.Column(db.String(500), nullable=True)
     recording_duration = db.Column(db.Integer, nullable=True)
@@ -12,10 +15,14 @@ class Call(db.Model):
     transcription_text = db.Column(db.Text, nullable=True)
     transcription_status = db.Column(db.String(20), nullable=True)
     
-    def __init__(self, id, from_phone, recording_url=None, recording_duration=None, 
-                 recording_status=None, transcription_text=None, transcription_status=None):
+    def __init__(self, id, from_phone, call_date, to_number=None, to_contact_name=None,
+                 recording_url=None, recording_duration=None, recording_status=None, 
+                 transcription_text=None, transcription_status=None):
         self.id = id
         self.from_phone = from_phone
+        self.call_date = call_date
+        self.to_number = to_number
+        self.to_contact_name = to_contact_name
         self.recording_url = recording_url
         self.recording_duration = recording_duration
         self.recording_status = recording_status
